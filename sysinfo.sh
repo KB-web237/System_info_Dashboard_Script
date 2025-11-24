01a2accd0905fb1758988b584b6195049b390a92
KBrand <kbrandn25@gmail.com>
2025-11-24 10:44:38 +0100

    Scripts to do display system info

---
+++ sysinfo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Current User: $(whoami | tr 'a-z' 'A-Z')"
+echo "Hostname: $(hostname)"
+echo "Current date/time: $(date)"
+echo "Full uptime string : $(uptime -p | sed 's/^up //')"
+echo "----------Memory Usage info----------------------------"
+echo "Total Memory: $(free -h | grep Mem: | awk '{print $2}')"
+echo "Used Memory: $(free -h | grep Mem: | awk '{print $3}')"
+echo "Free Memory: $(free -h | grep Mem: | awk '{print $4}')"
+
+echo "----------Disk Usage info----------------------------"
+#df  -h --total | grep 'total' | awk '{print "Total Disk space: "$2"\nUsed Disk space: "$3"\nFree Disk space: "$4}'
+echo "Total Disk space: $(df -h --total | grep 'total' | awk '{print $2}')"
+echo "Used Disk space: $(df -h --total | grep 'total' | awk '{print $3}')"
+echo "Free Disk space: $(df -h --total | grep 'total' | awk '{print $4}')"
+echo "----------------Running Processes info-----------------"
+echo "Total Running Processes: $(ps aux | wc -l)"
+echo "top 5 memory-consuming processes:"
+ps aux --sort=-%mem | head -n 6 | awk '{print $1, $2, $4, $11}'
