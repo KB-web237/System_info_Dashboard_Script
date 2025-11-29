b558dbea8d32a166335b26642261afc7244dc84d
KBrand <kbrandn25@gmail.com>
2025-11-29 14:05:18 +0100

    output design improved and requested changes corrected

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
-echo "----------------System Information---------------------"
+echo "======================System Information======================"
 user_name=$(whoami)
-echo "User: ${user_name^^}"
-echo "Hostname: $(hostname)"
-echo "Current date/time: $(date '+%a %b %e %H:%M:%S')"
+echo "User: ${user_name^^}   " "Host: $(hostname)"
+echo "Date: $(date '+%a %b %e %H:%M:%S')"
 echo "------------------Uptime Information---------------------"
 echo "Full uptime string : $(uptime -p | sed 's/^up //')"
-echo "----------Memory Usage info(GB)----------------------------"
-echo "Total Memory: $(free -h | grep Mem: | awk '{print $2}')"
-echo "Used Memory: $(free -h | grep Mem: | awk '{print $3}')"
-echo "Free Memory: $(free -h | grep Mem: | awk '{print $4}')"
-
-echo "----------Disk Usage info(GB)----------------------------"
+echo "----------Memory Usage (MB)----------------------------"
+echo "Total: $(free -m | grep Mem: | awk '{print $2}') | " "Used: $(free -m | grep Mem: | awk '{print $3}') |" "Free Memory: $(free -m | grep Mem: | awk '{print $4}')"
+echo "----------Disk Usage (MB)----------------------------"
 #df  -h --total | grep 'total' | awk '{print "Total Disk space: "$2"\nUsed Disk space: "$3"\nFree Disk space: "$4}'
-echo "Total Disk space: $(df -h --total | grep 'total' | awk '{print $2}')"
-echo "Used Disk space: $(df -h --total | grep 'total' | awk '{print $3}')"
-echo "Free Disk space: $(df -h --total | grep 'total' | awk '{print $4}')"
-echo "----------------Running Processes info-----------------"
+echo "Total: $(df -m --total | grep 'total' | awk '{print $2}') | " "Used: $(df -m --total | grep 'total' | awk '{print $3}') |" "Free: $(df -m --total | grep 'total' | awk '{print $4}')"
+echo "----------------Processes-----------------"
 echo "Total Running Processes: $(ps aux | wc -l)"
+echo "----------------Top 5 CPU-consuming processes-----------------"
 echo "top 5 memory-consuming processes:"
 ps aux --sort=-%mem | head -n 6 | awk '{print $1, $2, $4, $11}'
